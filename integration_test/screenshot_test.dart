1af031c946061b1a8aae9530d6cea1199c3311e7
joeshwoa <joeshwoa.george@gmail.com>
2024-12-25 18:31:53 +0200

    edit codemagic automated screenshots taker.

--- integration_test/screenshot_test.dart
+++ integration_test/screenshot_test.dart
@@ -1,10 +1,8 @@
-import 'dart:developer';
 import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:fixer_app/main.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:integration_test/integration_test_driver_extended.dart';
 
 
 
@@ -35,18 +33,4 @@ Future<void> main() async {
     // Take the screenshot
     await binding.takeScreenshot('screenshot-$platformName');
   });
-
-  try {
-    await integrationDriver(
-      onScreenshot: (screenshotName, screenshotBytes, [args]) async {
-        final File image = await File('screenshots/$screenshotName.png')
-            .create(recursive: true);
-        image.writeAsBytesSync(screenshotBytes);
-        return true;
-      },
-    );
-  } catch (e) {
-    log('Error occured: $e');
-  }
-
 }
